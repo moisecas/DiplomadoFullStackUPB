c4a6ce58c53fd2511bf3ac081caa1ff07db6bc47
Your Name <moisesjuniorcas20@gmail.com>
2022-03-23 20:49:21 -0500

    repaso general de html y resolución de ejercicios

--- Clase5/ejercicios5.js
+++ Clase5/ejercicios5.js
@@ -7,10 +7,30 @@
 // Usando un bucle, console.log todos los valores en el objeto de programación.
 
 
-let programación = {
+let programacion = {
     lenguajes : [ " JavaScript " , " Python " , " Ruby " ],
-    desafiante : verdadero ,
-    gratificante : verdadero ,
+    desafiante : true,
+    gratificante : true,
     dificultad : 8 ,
     chistes : " http://stackoverflow.com/questions/234075/what-is -su-mejor-programador-broma "
 };
+
+//accedemos al objeto y agregamos 
+programacion.lenguajes.push("go")
+console.log(programacion)
+
+//cambiar a dificultad 7 
+programacion.dificultad=7
+console.log(programacion) 
+
+//delete palabra
+delete programacion.chistes 
+console.log(programacion) 
+
+//iterar matriz idiomas 
+for (let i = 0; i < programacion.lenguajes.length; i++){
+    console.log(programacion.lenguajes[index]); 
+}
+for (let key in programacion){ //entre of y in, of para arreglos y in para objetos 
+    console.log(key) 
+}
