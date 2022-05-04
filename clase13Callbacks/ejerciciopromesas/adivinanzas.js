3b1407e0e3877a907648a265dcf06e7ef75deb11
Your Name <moisesjuniorcas20@gmail.com>
2022-05-04 16:43:20 -0500

    posible solución a ejercicio

---
+++ clase13Callbacks/ejerciciopromesas/adivinanzas.js
@@ -0,0 +1,20 @@
+alert("Adivinanzas usando promesas"); 
+
+let num =  prompt("Ingrese un numero: ")
+
+
+function mostrarnumeros(num) {
+    return new Promise(function(resolve, reject) {
+        if(num > 0) {
+            let numero =  Math.floor(Math.random() * 6);   
+            resolve(alert(numero)); //resolviendo la promesa
+        if(num == numero) {
+                resolve(alert("Los numeros son iguales")); //rechazando la promesa
+            }
+        } else {
+            reject(alert("el numero es negativo: " + num));//rechazando la promesa  
+        }
+        alert("Numero ingresado: " + num); 
+    });
+}
+mostrarnumeros(num) 
