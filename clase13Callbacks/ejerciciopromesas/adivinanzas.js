01483ed53d655053c47dee93dadfaa4268a58d70
Your Name <moisesjuniorcas20@gmail.com>
2022-05-09 10:45:29 -0500

    resolución ejercicio adivinanzas con promesas

--- clase13Callbacks/ejerciciopromesas/adivinanzas.js
+++ clase13Callbacks/ejerciciopromesas/adivinanzas.js
@@ -9,10 +9,14 @@ function mostrarnumeros(num) {
             let numero =  Math.floor(Math.random() * 6);   
             resolve(alert(numero)); //resolviendo la promesa
         if(num == numero) {
-                resolve(alert("Los numeros son iguales")); //resolviendo la promesa
+            
+            let puntaje = 2 
+            let acumulador = puntaje + contador++;
+            alert("Felicidades, acertaste el numero y su puntaje es de: " + acumulador);
+            resolve(alert("Los numeros son iguales")); //resolviendo la promesa
                 while(numero == num) {
                     contador++;
-                    alert("intentos: " + contador);
+                    alert("intentos acertados: " + contador);
                     let num =  prompt("Ingrese un numero entre 1 y 6: ")
                     mostrarnumeros(num); 
                 }
