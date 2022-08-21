83431d79b70ceb2d4ef04fcc6c906bc884def0d4
Your Name <moisesjuniorcas20@gmail.com>
2022-08-21 17:54:34 -0500

    probando consultas

---
+++ FullStackMinTic_2022/clase7_POO_JS/main.sql
@@ -0,0 +1,9 @@
+consultar base de datos personas
+select * from personas; 
+
+//de la tabla personas y clientes mostrar personas menores de 30 años y tabla clientes que compraron mas de dos productos
+select * from personas where edad < 30; 
+
+Consulta Tabla clientes nombre inicia m and tabla empleado edad > 30 años
+select * from clientes where nombre like 'm%'; 
+select * from empleados where edad > 30;
