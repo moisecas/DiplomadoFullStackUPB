b35c3304857a734e138846f783c3d35c53486293
Your Name <moisesjuniorcas20@gmail.com>
2022-10-02 17:27:33 -0500

    arrays filter

--- FullStackMinTic_2022/clase10JSArrays/array.js
+++ FullStackMinTic_2022/clase10JSArrays/array.js
@@ -22,3 +22,50 @@ console.log("*****arrow function*******");
 
 //for each con arrow function
 employees.forEach(employee => console.log(employee)); //recibe employee como parametro y lo imprime
+
+//array de objetos (JSON) empleado
+var employees2 = [
+    { name: "John", age: 30, city: "New York", enterprise: "Google" },
+    { name: "Mary", age: 28, city: "London", enterprise: "Facebook" },
+    { name: "Bob", age: 32, city: "Paris", enterprise: "Microsoft" },
+    { name: "Jane", age: 25, city: "New York", enterprise: "Google" },
+    { name: "Peter", age: 29, city: "London", enterprise: "lenovo" },
+    { name: "Susan", age: 33, city: "Paris", enterprise: "apple" },
+    { name: "Mark", age: 26, city: "New York", enterprise: "Google" },
+]
+
+console.log("*****for each con arrow function ver enterprise*******");
+employees2.forEach(employee => console.log(employee.name + " " + employee.enterprise)) //recibe employee como parametro y lo imprime
+
+console.log("*****name city*******");
+const nombreciudad=[];
+employees2.forEach(employee => nombreciudad.push(employee.name + " " + employee.city)) //recibe employee como parametro y lo imprime en el array nombreciudad
+console.log(nombreciudad); //push agrega al final del array 
+
+
+//metodo map para recorrer array 
+console.log("*****metodo map*******");
+const nombreciudad2 = employees2.map(employee => employee.name + " " + employee.city); 
+console.log(nombreciudad2); //muestra el array nombreciudad2
+//recibe employee como parametro y lo imprime en el array nombreciudad  
+
+const nombres = employees2.map(employee => { 
+    return nombreciudad2.push(employee.name + " " + employee.city)
+   
+}); //recibe employee como parametro y lo imprime en el array nombres
+
+console.log("*****enterprise microsoft*******");
+//for
+for (var i = 0; i < employees2.length; i++) {
+    if (employees2[i].enterprise == "Microsoft") { //si enterprise es igual a microsoft imprime
+        console.log(employees2[i].name); //imprime el nombre del empleado que trabaja en microsoft 
+    }
+}
+//filter
+console.log("*****filter enterprise microsoft*******");
+const microsoft = employees2.filter(employee => employee.enterprise == "Microsoft"); //recibe employee como parametro y lo imprime en el array microsoft
+
+//filter para recorrer array
+console.log("*****filter enterprise google name*******");
+const empleadoGoogle = employees2.filter(employee => employee.enterprise == "Google"); //recibe employee como parametro y lo imprime en el array empleadoGoogle
+console.log(empleadoGoogle); //muestra el array empleadoGoogle 
