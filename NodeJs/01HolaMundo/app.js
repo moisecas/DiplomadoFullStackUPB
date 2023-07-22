404b3cbfef960a26463f1b69a1af0498991735dc
unknown <moisesjuniorcas20@gmail.com>
2023-07-21 20:48:27 -0500

    repasando node

--- NodeJs/01HolaMundo/app.js
+++ NodeJs/01HolaMundo/app.js
@@ -1,2 +1,13 @@
-let name =  "moises"
-console.log(`Hola ${name}`) // Hola moises
+// let name =  "moises"
+
+// name = "david" 
+//permnite ser renombrada por ser declarada    con let
+
+// console.log(`Hola ${name}`) // 
+
+
+const nombre = "moises" 
+
+nombre = "david" // no permite ser renombrada por ser declarada    con const
+
+console.log(`Hola ${nombre}`) // genera error por que no se puede renombrar una constante
